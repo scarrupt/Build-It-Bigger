d1f3b3bc3dbf90dc5ae73702d2fb287d57e0c662
Steve <steve@codefactoring.com>
2015-10-29 23:14:45 +0100

    test: configure Test Task and correct test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -8,11 +8,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import codefactoring.com.jokeviewer.JokeView;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasClassName;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
@@ -26,7 +29,7 @@ public class ApplicationTest{
     @Test
     public void retrieveJokeOnClick() {
         onView(withId(R.id.tell_joke_button)).perform(click());
-        intended(toPackage("com.udacity.gradle.builditbigger"));
+        intended(hasComponent(hasClassName(JokeView.class.getName())));
         intended(hasExtra(equalTo(Intent.EXTRA_TEXT), notNullValue()));
     }
 }
